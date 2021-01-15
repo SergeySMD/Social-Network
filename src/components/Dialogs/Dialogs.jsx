1010168d5193022da3d6d69cec05e0ebef96a832
SergeySMD <segit@mail.ru>
2021-01-15 14:24:46 +0300

    dialogs page

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+let Dialogs = (props) => {
+    return (
+        <div>
+            Dialogs
+        </div>
+    );
+}
+export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,23 +3,23 @@ import s from './Navbar.module.css';
 
 let Navbar = () => {
     return (
-      <nav className={s.nav}>
-        <div className={`${s.item} ${s.active}`}>
-          <a>My page</a>
-        </div>
-        <div className={s.item}>
-          <a>Friends</a>
-        </div>
-        <div className={s.item}>
-          <a>Messages</a>
-        </div>
-        <div className={s.item}>
-          <a>News feed</a>
-        </div>
-        <div className={s.item}>
-          <a>Settings</a>
-        </div>
-      </nav>
+        <nav className={s.nav}>
+            <div className={`${s.item} ${s.active}`}>
+                <a>My page</a>
+            </div>
+            <div className={s.item}>
+                <a>Messages</a>
+            </div>
+            <div className={s.item}>
+                <a>Friends</a>
+            </div>
+            <div className={s.item}>
+                <a>News feed</a>
+            </div>
+            <div className={s.item}>
+                <a>Settings</a>
+            </div>
+        </nav>
     )
 }
 export default Navbar;
